59c3a88d744eced6a314d2c27fc549daea9da6b7
Xiyang Lyu <xily00001@stud.uni-saarland.de>
2024-06-10 16:39:04 +0200

    add comment about counter

--- src/main/scala/counter/Counter.scala
+++ src/main/scala/counter/Counter.scala
@@ -5,22 +5,24 @@ import chisel3.util._
 
 class Counter(val bitWidth: Int) extends Module {
     val io = IO(new Bundle{
-        val c = Input(Bool())
-        val l = Input(Bool())
-        val x = Input(UInt(bitWidth.W))
-        val y = Output(UInt(bitWidth.W))
+        val c = Input(Bool())       // clear
+        val l = Input(Bool())       // load
+        val x = Input(UInt(bitWidth.W))     // load value
+        val y = Output(UInt(bitWidth.W))    // output
     })
+    // initialize a register, has a default value 0
     val register = RegInit(0.U(bitWidth.W))
+    // wire, define an unsigned int wire with the same width as register
     val incout = Wire(UInt(bitWidth.W))
         
     when (!io.l) {
-        register := io.x
+        register := io.x        //低电平有效，如果l=0，那么register存入io.x的值
     } .elsewhen (!io.c) {
-        register := 0.U
+        register := 0.U         //低电平有效，如果c=0，那么register的值清零
     } .otherwise {
-        register := incout
+        register := incout      //register的值加1
     }
     
     io.y := register
-    incout := register + 1.U
+    incout := register + 1.U    //incrementer
 }
--- src/test/scala/counter/testCounter.scala
+++ src/test/scala/counter/testCounter.scala
@@ -14,13 +14,16 @@ class testCounter extends AnyFreeSpec with Matchers {
     "Counter should increment correctly" in {
         simulate(new Counter(4)) { dut => 
             val bitWidth = dut.bitWidth
-            dut.io.c.poke(0.B)
-            dut.io.l.poke(1.B)
-            dut.clock.step()
-            dut.io.c.poke(1.B)
-            for (i <- 0 until (1 << bitWidth)) {
+            dut.io.c.poke(0.B)          // c=0, clear, register = 0
+            dut.io.l.poke(1.B)          // l=1
+            dut.clock.step()            // 使时钟前进一个周期
+            dut.io.c.poke(1.B)          // c=1,l=1, 此时正常计数
+            for (i <- 0 until (1 << bitWidth)) {    //0-2^4-1
                 dut.io.y.expect(i.U)
-                dut.clock.step()
+
+                println(dut.io.y)
+
+                dut.clock.step()        //every step move, the counter will add 1
             }
             println("Success")
         }
@@ -29,17 +32,19 @@ class testCounter extends AnyFreeSpec with Matchers {
     "Counter should increment correctly2" in {
         simulate(new Counter(4)) { dut => 
             val bitWidth = dut.bitWidth
-            val maxVal = 1 << bitWidth
+            val maxVal = 1 << bitWidth // 2^4
             val rand = new Random
             dut.io.c.poke(1.B)
             //numTests: 20
             for (_ <- 0 until 20) {
                 val x = rand.nextInt(maxVal)
+                println(s"maxVal = $x")
                 dut.io.l.poke(0.B)
                 dut.io.x.poke(x.U)
-                dut.clock.step()
-                dut.io.l.poke(1.B)
+                dut.clock.step()        //每次都load random number 到register
+                dut.io.l.poke(1.B)      //取消load, 然后继续计数
                 for (i <- x until maxVal) {
+                    println(s"i = $i")
                     dut.io.y.expect(i.U)
                     dut.clock.step()
                 }
